b19c4b185f7e7139139b2b8b22c1269a419303a9
kazutotakeuchi-32 <kazutotakeuchi32@gmail.com>
2022-06-12 22:16:18 +0900

    phpコンテナ->dbコンテナ 接続成功

--- src/index.php
+++ src/index.php
@@ -1,5 +1,29 @@
 <?php 
+
+class Db
+{
+  public static function connect()
+  {
+    try {
+      $dsn = "mysql:dbname=test;host=db;";
+      $user = "root";
+      $password = "password";
+      $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      ];
+      $dbh = new PDO($dsn, $user, $password, $options);
+      $dbh->query('SET NAMES utf8');
+      return $dbh;
+    } catch (\Throwable $th) {
+      throw $th;
+      var_dump($th);
+    }
+  }
+}
   $test = "name";
+  $db = Db::connect();
+  var_dump($db);
 ?>
 <!DOCTYPE html>
 <html lang="en">
