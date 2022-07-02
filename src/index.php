4d5c3ce86f69a41f03cc5bc98325493edf0b2a18
kazutotakeuchi-32 <kazutotakeuchi32@gmail.com>
2022-07-02 14:54:27 +0900

    ディレクトリ 修正

--- src/index.php
+++ src/index.php
@@ -53,8 +53,8 @@ require("vendor/autoload.php");
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" href="/style/index.css">
-  <script src="/js/main.js" defer></script>
+  <link rel="stylesheet" href="/index.css">
+  <script src="/main.js" defer></script>
 </head> 
 <body>
   <p class="text"></p>
